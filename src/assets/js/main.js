b9a2b5931371a5da547e4260abf72fc29a3fe0fe
Paweł Brzozowski <pawel@brzoza.net>
2023-06-06 15:49:12 +0200

    Fetch products

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,50 +2,55 @@ import { createAppbar } from "../../components/appbar";
 import { createCard } from "../../components/card";
 import { createCart } from "../../components/cart";
 
+const API_URL = "http://localhost:3000";
+
 const rootElement = document.getElementById("root");
+
+// const fetchProducts = () => {
+//   fetch(`${API_URL}/items`)
+//     .then((response) => response.json())
+//     .then((products) => console.log(products));
+// };
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(`${API_URL}/items`);
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const buildProducts = () => {
+  fetchProducts()
+    .then((products) => {
+      const cards = document.createElement("div");
+      cards.classList.add("flex", "flex-wrap", "justify-center", "gap-4");
+      products.forEach((product) => {
+        const card = document.createElement("div");
+        card.classList.add("max-w-[30%]");
+        card.innerHTML = createCard({
+          name: product.name,
+          image: product.images[0],
+          price: product.price,
+        });
+        cards.append(card);
+      });
+      rootElement.appendChild(cards);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const buildAppUI = () => {
   const appbar = document.createElement("div");
   const cards = document.createElement("div");
-  const card1 = document.createElement("div");
-  const card2 = document.createElement("div");
-  const card3 = document.createElement("div");
-  const card4 = document.createElement("div");
-
-  cards.classList.add("flex", "flex-wrap", "justify-center", "gap-4");
 
   appbar.innerHTML = createAppbar({ cartElement: createCart() });
-
-  card1.classList.add("max-w-[30%]");
-  card1.innerHTML = createCard({
-    name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
-    image: "https://flowbite.com/docs/images/products/apple-watch.png",
-    price: 599,
-  });
-
-  card2.classList.add("max-w-[30%]");
-  card2.innerHTML = createCard({
-    name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
-    image: "https://flowbite.com/docs/images/products/apple-watch.png",
-    price: 599,
-  });
-
-  card3.classList.add("max-w-[30%]");
-  card3.innerHTML = createCard({
-    name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
-    image: "https://flowbite.com/docs/images/products/apple-watch.png",
-    price: 599,
-  });
-
-  card4.classList.add("max-w-[30%]");
-  card4.innerHTML = createCard({
-    name: "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
-    image: "https://flowbite.com/docs/images/products/apple-watch.png",
-    price: 599,
-  });
-
-  cards.append(card1, card2, card3, card4);
   rootElement.append(appbar, cards);
 };
 
 buildAppUI();
+buildProducts();
